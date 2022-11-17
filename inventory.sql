f693f00a28200bf6394325c896c5bf7a4e0c80df
jrobin15283 <jeremyakiva@gmail.com>
2022-11-17 12:27:40 -0600

    Update inventory.sql

--- inventory.sql
+++ inventory.sql
@@ -2,12 +2,13 @@ CREATE TABLE Inventory(
     ITEM_ID int not null,
     ITEM_NAME char(50) not null,
     ITEM_COST decimal(4, 2) not null,
+    ITEM_QTY int,
     primary key(ITEM_ID)
 );
 
 INSERT INTO Inventory VALUES
-    (1, "Shirt", 9.99),
-    (2, "Jeans", 19.99),
-    (3, "Socks", 4.99),
-    (4, "Sweater", 29.99),
-    (5, "Sweatshirt", 49.99);
+    (1, "Shirt", 9.99, 50),
+    (2, "Jeans", 19.99, 50),
+    (3, "Socks", 4.99, 50),
+    (4, "Sweater", 29.99, 50),
+    (5, "Sweatshirt", 49.99, 50);
