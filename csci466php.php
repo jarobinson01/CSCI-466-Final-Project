84879e73f053cb22821122b3e6883c1d4eea43d7
jrobin15283 <jeremyakiva@gmail.com>
2022-11-17 11:33:03 -0600

    starting files and main website

---
+++ csci466php.php
@@ -0,0 +1,27 @@
+<?php
+include("passwords.php");
+
+$dsn = "mysql:host=courses;dbname=z1934222";
+$pdo = new PDO($dsn, username, password);
+
+function draw_table($rows) {
+  echo "<table border=1 cellspacing=1>";
+  echo "<tr>";
+
+  foreach($rows[0] as $key => $item) {
+    echo "<th>$key</th>";
+  }
+
+  foreach($rows as $row) {
+    echo "<tr>";
+    foreach($row as $item) {
+      echo "<td>$item</td>";
+    }
+    echo "</tr>";
+  }
+}
+
+
+
+echo "</table>";
+?>
