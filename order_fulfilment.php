85212b862a0fe335e7a930734ae0e3b61677b113
jrobin15283 <jeremyakiva@gmail.com>
2022-11-28 11:25:36 -0600

    order

--- order_fulfilment.php
+++ order_fulfilment.php
@@ -9,7 +9,7 @@ $rs = $pdo->query("SELECT * FROM Orders;");
 $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
 draw_table($rows);
 
-echo "<form action='main_page.php method='post'>";
+echo "<form action='main_page.php'>";
 echo "<input type='submit' value='Home'/>";
 echo "</form>";
 ?>
