9d4aa0a8f7e0e799174034cc07f452f4eb4aa901
jrobin15283 <jeremyakiva@gmail.com>
2022-11-29 14:24:11 -0600

    Update fulfill_order.php

--- fulfill_order.php
+++ fulfill_order.php
@@ -13,6 +13,7 @@ $fulfill->execute(array($tracking));
 $rs = $pdo->query("SELECT * FROM Orders;");
 $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
 draw_order_table($rows);
+echo $tracking;
 
 echo "<form action='main_page.php'>";
 echo "<input type='submit' value='Home'/>";
