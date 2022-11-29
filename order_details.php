9e08e822020e0bf60d5fb1c86e8bde3f9f1a5767
jrobin15283 <jeremyakiva@gmail.com>
2022-11-29 15:02:17 -0600

    Update order_details.php

--- order_details.php
+++ order_details.php
@@ -1,4 +1,23 @@
 <?php
 $tracking = key($_POST);
-echo $tracking;
+
+include("passwords.php");
+include("functions.php");
+
+$dsn = "mysql:host=courses;dbname=z1934222";
+$pdo = new PDO($dsn, $username, $password);
+
+$fulfill = $pdo->prepare("SELECT * FROM Orders WHERE TRACKING_NUM = ?;");
+$fulfill->execute(array($tracking));
+
+$rows = $fulfill->fetchAll(PDO::FETCH_ASSOC);
+draw_order_table($rows);
+
+echo "<form action='main_page.php'>";
+echo "<input type='submit' value='Home'/>";
+echo "</form>";
+
+echo "<form action='order_info_page.php'>";
+echo "<input type='submit' value='Back'/>";
+echo "</form>";
 ?>
