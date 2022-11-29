66294ee50e08de89cef066c92987b6e37f8913e7
jrobin15283 <jeremyakiva@gmail.com>
2022-11-29 14:25:01 -0600

    Update fulfill_order.php

--- fulfill_order.php
+++ fulfill_order.php
@@ -8,12 +8,11 @@ $dsn = "mysql:host=courses;dbname=z1934222";
 $pdo = new PDO($dsn, $username, $password);
 
 $fulfill = $pdo->prepare("SELECT * FROM Orders WHERE TRACKING_NUM = ?;");
-$fulfill->execute(array($tracking));
+$fulfill->execute($tracking);
 
 $rs = $pdo->query("SELECT * FROM Orders;");
 $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
 draw_order_table($rows);
-echo $tracking;
 
 echo "<form action='main_page.php'>";
 echo "<input type='submit' value='Home'/>";
