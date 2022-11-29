997fd93a3385f3f6eeb79eb37b1a3b000fed65b0
jrobin15283 <jeremyakiva@gmail.com>
2022-11-29 07:52:01 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ function draw_product_table($rows) {
     foreach($row as $item) {
       echo "<td>$item</td>";
     }
-    $qty = array($row)[3];
+    $qty = $row['ITEM_QTY'];
 
     echo "<td><button>Select This Item</button></td>";
     echo "<td>Qty: ".$qty."<input type='number' min='1' value='1'></td>";
