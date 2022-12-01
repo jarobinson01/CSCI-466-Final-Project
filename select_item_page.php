827fe873e6ff50eea6d3642daa4c3eec44b78d1d
Julian Sanchez <z1933999@turing.cs.niu.edu>
2022-12-01 17:09:48 -0600

    buttons wokring

--- select_item_page.php
+++ select_item_page.php
@@ -1,15 +1,27 @@
 <?php
+
+$tracking = key($_POST);
+
 include("passwords.php");
 include("functions.php");
 
+$dsn = "mysql:host=courses;dbname=z1934222";
+$pdo = new PDO($dsn, $username, $password);
+
+$rs = $pdo->query("SELECT * FROM Inventory WHERE Item_ID;");
+$rows = $rs->fetchAll(PDO::FETCH_ASSOC);
+draw_product_table($rows);
+
 echo "<form action='main_page.php'>";
 echo "<input type='submit' value='Home'/>";
-echo "</form><br>";
+echo "</form>";
 
 echo "<h1>Item Selected</h1><br>";
 
 
-$dsn = "mysql:host=courses;dbname=z1934222";
-$pdo = new PDO($dsn, $username, $password);
+
+
+
+
 
 ?>
