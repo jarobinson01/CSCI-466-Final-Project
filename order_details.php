69e078c783836d93ac4fe3446ea2b686e731fb0c
jrobin15283 <jeremyakiva@gmail.com>
2022-12-01 22:46:12 -0600

    Update order_details.php

--- order_details.php
+++ order_details.php
@@ -14,8 +14,6 @@ $fulfill->execute(array($tracking));
 $rows = $fulfill->fetchAll(PDO::FETCH_ASSOC);
 draw_order_table($rows);
 
-echo "<h1>Customer Name: <h2>"
-
 echo "<form action='main_page.php'>";
 echo "<input type='submit' value='Home'/>";
 echo "</form>";
