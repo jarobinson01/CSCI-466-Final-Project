48b0f024209351eebf1e9e3e3c588ddd584f746d
jrobin15283 <jeremyakiva@gmail.com>
2022-12-01 23:19:09 -0600

    Update order_confirmation.php

--- order_confirmation.php
+++ order_confirmation.php
@@ -33,10 +33,9 @@ foreach($rows as $row) {
     $total += $row['ITEM_COST'] * $row['ITEM_QTY'];
 }
 
-$find_customer = $pdo->prepare("SELECT * FROM Customers WHERE EMAIL = '?';");
+$find_customer = $pdo->prepare("SELECT * FROM Customers WHERE CUST_EMAIL = '?';");
 $find_customer->execute(array($email));
 $rows = $find_customer->fetchAll(PDO::FETCH_ASSOC);
-draw_table($rows);
 
 foreach($rows as $row) {
     $cust_id += $row['CUST_ID'];
