30f1bda60623572c01ea14066c3954c623513789
jrobin15283 <jeremyakiva@gmail.com>
2022-12-01 22:34:20 -0600

    Update order_confirmation.php

--- order_confirmation.php
+++ order_confirmation.php
@@ -19,6 +19,28 @@ echo "<h2>Thanks for purchasing, ".$fname."!<h2><br>";
 $dsn = "mysql:host=courses;dbname=z1934222";
 $pdo = new PDO($dsn, $username, $password);
 
+// add customer to database
 $add_customer = $pdo->prepare("INSERT IGNORE INTO Customers(F_NAME, L_NAME, EMAIL) VALUES('?', '?', '?');");
 $add_customer->execute(array($fname, $lname, $email));
+
+// add order to database
+$total = 0;
+$cust_id = "";
+$rs = $pdo->query("SELECT ITEM_COST, ITEM_QTY FROM Cart;");
+$rows = $rs->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($rows as $row) {
+    $total += $row['ITEM_COST'] * $row['ITEM_QTY'];
+}
+
+$find_customer = $pdo->prepare("SELECT * FROM Customers WHERE EMAIL = ?;");
+$rs = $find_customer->execute(array($email));
+$rows = $rs->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($rows as $row) {
+    $cust_id += $row['CUST_ID'];
+}
+
+$add_order = $pdo->prepare("INSERT IGNORE INTO Orders(CUST_ID, ORDER_COST) VALUES('?', '?', 'PROCESSING');");
+$add_customer->execute(array($cust_id, $total));
 ?>
